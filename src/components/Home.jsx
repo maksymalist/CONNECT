5c14258a86966eed525f9309e33c3446cb20aa25
John8790909 <johnsmith8790909@gmail.com>
2021-08-23 09:03:52 -0400

    homepage changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,29 +21,21 @@ export default function HomePage() {
             <br></br>
             <br></br>
             <h1 className='homepage-text'>CONNECT!</h1>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
             <div id='homePageDivWhite' className='homePageDivWhite'>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <br></br><br></br><br></br><br></br>
             <h2 className='homepage-text-medium'>Take learning to the <br/> next level.</h2>
             <br /><br /><br /><br /><br /><br /><br />
             <h3 className='homepage-text-small'>CONNECT! helps you learn faster <br /> and together.</h3>
             <br /><br /><br /><br /><br />
             <Button style={{zIndex:'2'}} id='button-start' variant="contained" color="primary" size='large' onClick={()=>{window.location = '/play'}}>Start Learning ➞</Button>
-            <br></br><br></br><br></br><br></br>
-            <img src={HomePageImage} className='imgDiv' id='home-page-img' alt='home-page-img'/>
-
+            <br></br><br></br><br></br><br></br><br></br><br></br>
+            <div className='imgDiv'>
+                <img src={HomePageImage} id='home-page-img' alt='home-page-img'/>
+            </div>
             </div>
         </div>
+        <br></br><br></br><br></br><br></br>
         <div className="newdiv2">
             <h1 className='aboutText' style={{color:'white', zIndex:'230'}}>About</h1>
             <div className='aboutText' style={{color:'white', zIndex:'230'}}>
