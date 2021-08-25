2408664fabee50bb2d540ee74ec8b439835c8950
John8790909 <johnsmith8790909@gmail.com>
2021-08-25 18:49:49 -0400

    Your Finished => You're Finished in FinishedScreen.jsx

--- src/components/FinishedScreen.jsx
+++ src/components/FinishedScreen.jsx
@@ -15,7 +15,7 @@ import '../style/style.css'
 export default function FinishedScreen({ match, user }) {
     return (
         <div style={{height:'100vh', width:'100%', display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
-            <h1 style={{color:'white'}}>Your done!</h1>
+            <h1 style={{color:'white'}}>You're done!</h1>
             <h2 style={{color:'white', textAlign:'center'}}>Wait For More People To Finish</h2>
             <CircularProgress size={150} thickness={3} style={{color:'white', margin:'100px'}}/>
             <div>
