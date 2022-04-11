e78ed99be94e3f16fa8e2c92d8344c353297958e
John Doe <79988159+John8790909@users.noreply.github.com>
2022-04-11 12:50:09 -0400

    🤝| added translations to login from️

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -93,66 +93,81 @@ function Login() {
 
   return (
     <div className="login-main-container">
-      <div className="login-component-container">
-        {step === 0 && (
-          <>
-            <div style={{ width: "250px", height: "250px" }}>
-              <Spline scene="https://draft.spline.design/x-auyQbM97sLc0dD/scene.spline" />
+      {step === 0 && (
+        <div className="login-component-container">
+          <br></br>
+          <Typography variant="h3" className="login-title">
+            <b style={{ color: "#6C63FF" }}>
+              {Translations[userLanguage].login.title}
+            </b>
+          </Typography>
+          <br></br>
+          <br></br>
+          <Typography variant="h6" className="login-title">
+            <b style={{ fontStyle: "italic" }}>
+              {Translations[userLanguage].login.title2}
+            </b>
+          </Typography>
+          <br></br>
+          <div
+            style={{ width: "250px", height: "250px", marginBottom: "40px" }}
+          >
+            <Spline scene="https://draft.spline.design/x-auyQbM97sLc0dD/scene.spline" />
+          </div>
+          <GoogleLogin
+            clientId="701696427912-ajmlkcj3hpo46q5fokhtn5mmeib0m3be.apps.googleusercontent.com"
+            buttonText={Translations[userLanguage].login.googlebutton}
+            onSuccess={responseGoogle}
+            onFailure={() => {
+              console.log(Error);
+            }}
+            cookiePolicy={"single_host_origin"}
+            style={{ backgroundColor: "#e0e0e0" }}
+            className="google-login-button"
+            type="large"
+          />
+        </div>
+      )}
+      {step === 1 && (
+        <div className="login-component-container-blank">
+          <Typography
+            variant="h3"
+            className="login-title"
+            style={{ color: "white" }}
+          >
+            <b>{Translations[userLanguage].login.role.title}</b>
+          </Typography>
+          <div style={{ width: "100%", height: "100px" }} />
+          <div className="role__img__wrapper">
+            <div className="role__img__card">
+              <Typography variant="h4" className="login-title">
+                <b>{Translations[userLanguage].login.role.teacher}</b>
+              </Typography>
+              <img
+                src={teacher}
+                alt="teacher"
+                className="login-role-img"
+                onClick={() => {
+                  handleRole("teacher", userObj);
+                }}
+              />
             </div>
-            <br></br>
-            <Typography variant="h4" className="login-title">
-              <b>{Translations[userLanguage].login.title}</b>
-            </Typography>
-            <br></br>
-            <GoogleLogin
-              clientId="701696427912-ajmlkcj3hpo46q5fokhtn5mmeib0m3be.apps.googleusercontent.com"
-              buttonText={Translations[userLanguage].login.googlebutton}
-              onSuccess={responseGoogle}
-              onFailure={() => {
-                console.log(Error);
-              }}
-              cookiePolicy={"single_host_origin"}
-              style={{ backgroundColor: "#e0e0e0" }}
-            />
-          </>
-        )}
-        {step === 1 && (
-          <div>
-            <Typography variant="h3" className="login-title">
-              <b>Select your role 👩‍🏫/🧑‍🎓</b>
-            </Typography>
-            <br></br>
-            <div className="role__img__wrapper">
-              <div className="role__img__card">
-                <Typography variant="h4" className="login-title">
-                  <b>Teacher</b>
-                </Typography>
-                <img
-                  src={teacher}
-                  alt="teacher"
-                  className="login-role-img"
-                  onClick={() => {
-                    handleRole("teacher", userObj);
-                  }}
-                />
-              </div>
-              <div className="role__img__card">
-                <Typography variant="h4" className="login-title">
-                  <b>Student</b>
-                </Typography>
-                <img
-                  src={student}
-                  alt="student"
-                  className="login-role-img"
-                  onClick={() => {
-                    handleRole("student", userObj);
-                  }}
-                />
-              </div>
+            <div className="role__img__card">
+              <Typography variant="h4" className="login-title">
+                <b>{Translations[userLanguage].login.role.student}</b>
+              </Typography>
+              <img
+                src={student}
+                alt="student"
+                className="login-role-img"
+                onClick={() => {
+                  handleRole("student", userObj);
+                }}
+              />
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
