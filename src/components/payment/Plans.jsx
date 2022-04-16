e13ef70da789af6b2905cc81ac5794fb715715b3
John Doe <79988159+John8790909@users.noreply.github.com>
2022-04-16 13:20:53 -0400

    ✨| made the /plans look way better

--- src/components/payment/Plans.jsx
+++ src/components/payment/Plans.jsx
@@ -5,14 +5,16 @@ import EntrepriseImg from "../../img/entreprise_sub.svg";
 
 import { toast } from "react-toastify";
 
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Button, Typography, Chip } from "@mui/material";
 
 import { CheckRounded } from "@mui/icons-material";
 import Translations from "../../translations/translations.json";
 
 import { useSelector } from "react-redux";
 import getUser from "../../hooks/getUser";
+
+import banner from "../../img/plansBanner.svg";
+import Wave from "../../img/WhiteBigStripe.svg";
 
 export default function Plans() {
   const user = getUser();
@@ -31,157 +33,353 @@ export default function Plans() {
   };
   useEffect(() => {
     if (user == null) return;
+    document.getElementById("root").style.padding = "0px";
+
+    return () => {
+      document.getElementById("root").style.padding = "10px";
+    };
   }, []);
 
   return (
-    <div className="planDiv">
-      <div id="plan1">
-        <h1>{Translations[userLanguage].plans.starter.title}</h1>
-        <h2>{Translations[userLanguage].plans.starter.price}</h2>
-        <h2>{Translations[userLanguage].plans.starter.features.title}</h2>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "start",
-          }}
-        >
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.starter.features.feature1}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.starter.features.feature2}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.starter.features.feature3}
-          </Typography>
-        </div>
-        <img
-          alt="starter-img"
-          height="220px"
-          width="220px"
-          src={StarterImg}
-        ></img>
-      </div>
-      <div id="plan2">
-        <h1>{Translations[userLanguage].plans.classroom.title}</h1>
-        <h2>
-          {Translations[userLanguage].plans.classroom.price}
-          <span style={{ fontSize: "15px" }}>
-            {Translations[userLanguage].plans.month}
-          </span>
-        </h2>
-        <h2>{Translations[userLanguage].plans.classroom.features.title}</h2>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "start",
-          }}
-        >
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.classroom.features.feature1}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.classroom.features.feature2}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.classroom.features.feature3}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.classroom.features.feature4}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.classroom.features.feature5}
-          </Typography>
-        </div>
-        <img
-          alt="classroom-img"
-          height="220px"
-          width="220px"
-          src={TeacherImg}
-        ></img>
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "auto",
+          minHeight: "350px",
+          display: "flex",
+          backgroundColor: "white",
+          padding: "10px",
+          justifyContent: "space-evenly",
+        }}
+      >
         <div>
-          <Button
-            id="Classroom"
-            className="sub-button"
-            style={{ marginBottom: "1vh" }}
-            variant="contained"
-            color="primary"
-            size="medium"
-            onClick={() => selectClassroomPlan()}
-          >
-            {plan === "Classroom"
-              ? Translations[userLanguage].plans.classroom.buttonsubscribed
-              : Translations[userLanguage].plans.classroom.button}
-          </Button>
+          <div>
+            <Typography
+              variant="h3"
+              style={{
+                marginBottom: "20px",
+                marginTop: "50px",
+                fontWeight: "bold",
+                textAlign: "left",
+                maxWidth: "600px",
+              }}
+            >
+              <Typography
+                variant="h3"
+                style={{
+                  fontWeight: "bold",
+                  color: "#6c63ff",
+                  marginBottom: "15px",
+                }}
+              >
+                {Translations[userLanguage].plans.title1}
+              </Typography>
+              {Translations[userLanguage].plans.title2}
+            </Typography>
+          </div>
+          <div></div>
         </div>
-      </div>
-      <div id="plan3">
-        <h1>{Translations[userLanguage].plans.entreprise.title}</h1>
-        <h2>
-          {Translations[userLanguage].plans.entreprise.price}
-          <span style={{ fontSize: "15px" }}>
-            {Translations[userLanguage].plans.month}
-          </span>
-        </h2>
-        <h2>{Translations[userLanguage].plans.entreprise.features.title}</h2>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "start",
-          }}
-        >
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.entreprise.features.feature1}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.entreprise.features.feature2}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.entreprise.features.feature3}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.entreprise.features.feature4}
-          </Typography>
-          <Typography variant="subtitle1" className="features">
-            {"✅ "}
-            {Translations[userLanguage].plans.entreprise.features.feature5}
-          </Typography>
-        </div>
-        <img
-          alt="entreprise-img"
-          height="220px"
-          width="220px"
-          src={EntrepriseImg}
-        ></img>
         <div>
-          <Button
-            className="sub-button"
-            style={{ marginBottom: "1vh" }}
-            variant="contained"
-            color="primary"
-            size="medium"
-          >
-            {plan === "Entreprise"
-              ? Translations[userLanguage].plans.entreprise.buttonsubscribed
-              : Translations[userLanguage].plans.entreprise.button}
-          </Button>
+          <img
+            src={banner}
+            alt="banner"
+            style={{
+              maxWidth: "400px",
+              maxHeight: "400px",
+              width: "100%",
+              height: "100%",
+            }}
+          />
         </div>
       </div>
-    </div>
+      <img
+        src={Wave}
+        alt="wave"
+        style={{
+          width: "100%",
+          height: "auto",
+        }}
+      />
+      <div className="planDiv">
+        <div id="plan1">
+          <Typography
+            variant="h3"
+            style={{ marginTop: "20px", marginBottom: "20px" }}
+          >
+            <b>{Translations[userLanguage].plans.starter.title}</b>
+          </Typography>
+          <Typography variant="h5" color="#4F5251">
+            {Translations[userLanguage].plans.starter.sub}
+          </Typography>
+          <br></br>
+
+          <Typography variant="h5" color="#4F5251">
+            {Translations[userLanguage].plans.starter.at} <br></br>{" "}
+            <span style={{ color: "#6c63ff", fontSize: "1.8em" }}>
+              {Translations[userLanguage].plans.starter.price}
+            </span>
+            <br></br>
+            {Translations[userLanguage].plans.starter.permonth}
+          </Typography>
+          <br></br>
+          <div
+            style={{ display: "flex", justifyContent: "center", width: "100%" }}
+          >
+            <img
+              alt="starter-img"
+              height="220px"
+              width="220px"
+              src={StarterImg}
+            />
+          </div>
+          <br></br>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "30px",
+            }}
+          >
+            <Button
+              className="sub-button"
+              style={{ paddingLeft: "35px", paddingRight: "35px" }}
+              variant={plan == "Starter" ? "outlined" : "contained"}
+              color="action"
+              size="large"
+              onClick={() => {
+                plan == "Starter"
+                  ? toast.info(
+                      Translations[userLanguage].alerts.alreadyhaveplan
+                    )
+                  : toast.info(Translations[userLanguage].alerts.betterplan);
+              }}
+            >
+              {plan === "Starter"
+                ? Translations[userLanguage].plans.classroom.buttonsubscribed
+                : Translations[userLanguage].plans.classroom.button}
+            </Button>
+          </div>
+          <div>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.starter.features.feature1}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.starter.features.feature2}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.starter.features.feature3}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.starter.features.feature4}
+            </Typography>
+          </div>
+        </div>
+        <div>
+          <div id="plan2">
+            <div
+              style={{
+                width: "100%",
+                height: "80px",
+                backgroundColor: "#1bb978",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "10px",
+                border: "2px solid black",
+                boxShadow: "5px 5px 0 #262626",
+              }}
+            >
+              <Typography variant="h5" style={{ color: "white" }}>
+                {Translations[userLanguage].plans.classroom.bestvalue}
+              </Typography>
+            </div>
+            <Typography
+              variant="h3"
+              style={{ marginTop: "20px", marginBottom: "20px" }}
+            >
+              <b>{Translations[userLanguage].plans.classroom.title}</b>
+            </Typography>
+
+            <Typography variant="h5" color="#4F5251">
+              {Translations[userLanguage].plans.classroom.sub}
+            </Typography>
+            <br></br>
+
+            <Typography variant="h5" color="#4F5251">
+              {Translations[userLanguage].plans.classroom.at} <br></br>{" "}
+              <span style={{ color: "#6c63ff", fontSize: "1.8em" }}>
+                {Translations[userLanguage].plans.classroom.price}
+              </span>
+              <br></br>
+              {Translations[userLanguage].plans.classroom.permonth}
+            </Typography>
+            <br></br>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <img
+                alt="classroom-img"
+                height="220px"
+                width="220px"
+                src={TeacherImg}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "30px",
+              }}
+            >
+              <Button
+                id="Classroom"
+                className="sub-button"
+                style={{ paddingLeft: "35px", paddingRight: "35px" }}
+                variant={plan === "Classroom" ? "outlined" : "contained"}
+                color="action"
+                size="large"
+                onClick={() => selectClassroomPlan()}
+              >
+                {plan === "Classroom"
+                  ? Translations[userLanguage].plans.classroom.buttonsubscribed
+                  : Translations[userLanguage].plans.classroom.button}
+              </Button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "start",
+              }}
+            >
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature1}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature2}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature3}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature4}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature5}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature6}
+              </Typography>
+              <Typography variant="subtitle1" className="features">
+                {"✅ "}
+                {Translations[userLanguage].plans.classroom.features.feature7}
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div id="plan3">
+          <Typography
+            variant="h3"
+            style={{ marginTop: "20px", marginBottom: "20px" }}
+          >
+            <b>{Translations[userLanguage].plans.entreprise.title}</b>
+          </Typography>
+          <Typography variant="h5" color="#4F5251">
+            {Translations[userLanguage].plans.entreprise.sub}
+          </Typography>
+          <br></br>
+
+          <Typography variant="h5" color="#4F5251">
+            {Translations[userLanguage].plans.entreprise.at} <br></br>{" "}
+            <span style={{ color: "#6c63ff", fontSize: "1.8em" }}>
+              {Translations[userLanguage].plans.entreprise.price}
+            </span>
+            <br></br>
+            {Translations[userLanguage].plans.entreprise.permonth}
+          </Typography>
+          <br></br>
+          <div
+            style={{ display: "flex", justifyContent: "center", width: "100%" }}
+          >
+            <img
+              alt="entreprise-img"
+              height="220px"
+              width="220px"
+              src={EntrepriseImg}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "30px",
+            }}
+          >
+            <Button
+              className="sub-button"
+              style={{ paddingLeft: "35px", paddingRight: "35px" }}
+              variant={plan === "Entreprise" ? "outlined" : "contained"}
+              color="action"
+              size="large"
+            >
+              {plan === "Entreprise"
+                ? Translations[userLanguage].plans.entreprise.buttonsubscribed
+                : Translations[userLanguage].plans.entreprise.button}
+            </Button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "start",
+            }}
+          >
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature1}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature2}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature3}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature4}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature5}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature6}
+            </Typography>
+            <Typography variant="subtitle1" className="features">
+              {"✅ "}
+              {Translations[userLanguage].plans.entreprise.features.feature7}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
