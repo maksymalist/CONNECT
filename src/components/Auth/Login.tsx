be3bfcded26ca3a1d18ed3cf19ff908c04290017
John Doe <79988159+John8790909@users.noreply.github.com>
2022-09-03 11:10:08 -0400

    🐛 | DEBUGGING: google oauth compatibility with new domain

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import GoogleLogin from "react-google-login";
-import { Typography } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import React, { useState } from 'react'
+import GoogleLogin from 'react-google-login'
+import { Typography } from '@mui/material'
+import { useLocation } from 'react-router-dom'
 //style
-import "../../style/loginStyles.css";
-import { useMutation, gql } from "@apollo/client";
-import axios from "axios";
-import config from "../../config.json";
+import '../../style/loginStyles.css'
+import { useMutation, gql } from '@apollo/client'
+import axios from 'axios'
+import config from '../../config.json'
 //role imgs
-import LoginArt from "../../img/Login/loginArt.svg";
-import teacher from "../../img/Login/teacher_role.svg";
-import student from "../../img/Login/student_role.svg";
-import useTranslations from "../../hooks/useTranslations";
+import LoginArt from '../../img/Login/loginArt.svg'
+import teacher from '../../img/Login/teacher_role.svg'
+import student from '../../img/Login/student_role.svg'
+import useTranslations from '../../hooks/useTranslations'
 //mutations
 const CREATE_USER = gql`
   mutation createUser(
@@ -31,44 +31,44 @@ const CREATE_USER = gql`
       role: $role
     )
   }
-`;
+`
 function Login() {
-  const translations = useTranslations();
-  const [createUser] = useMutation(CREATE_USER);
-  const search = useLocation().search;
-  const Gamecode = new URLSearchParams(search).get("code");
-  const Secret = new URLSearchParams(search).get("secret");
-  const EmoteId = new URLSearchParams(search).get("emoteId");
-  const [step, setStep] = useState(0);
-  const [userObj, setUserObj] = useState({});
+  const translations = useTranslations()
+  const [createUser] = useMutation(CREATE_USER)
+  const search = useLocation().search
+  const Gamecode = new URLSearchParams(search).get('code')
+  const Secret = new URLSearchParams(search).get('secret')
+  const EmoteId = new URLSearchParams(search).get('emoteId')
+  const [step, setStep] = useState(0)
+  const [userObj, setUserObj] = useState({})
   const responseGoogle = async (response: any) => {
     try {
-      const res = await axios.post(`${config["api-server"]}/user`, {
+      const res = await axios.post(`${config['api-server']}/user`, {
         userId: response.profileObj.googleId,
-      });
+      })
       if (res.data) {
         if (Gamecode) {
-          localStorage.setItem("user", JSON.stringify(response));
-          window.location.reload();
-          window.location = `/play?code=${Gamecode}` as any;
+          localStorage.setItem('user', JSON.stringify(response))
+          window.location.reload()
+          window.location = `/play?code=${Gamecode}` as any
         } else if (Secret && EmoteId) {
-          localStorage.setItem("user", JSON.stringify(response));
-          window.location.reload();
+          localStorage.setItem('user', JSON.stringify(response))
+          window.location.reload()
           window.location =
-            `/claim-emote?secret=${Secret}&emoteId=${EmoteId}` as any;
+            `/claim-emote?secret=${Secret}&emoteId=${EmoteId}` as any
         } else {
-          localStorage.setItem("user", JSON.stringify(response));
-          window.location.reload();
-          window.location.href = "/play";
+          localStorage.setItem('user', JSON.stringify(response))
+          window.location.reload()
+          window.location.href = '/play'
         }
       } else {
-        setStep(1);
-        setUserObj(response);
+        setStep(1)
+        setUserObj(response)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   const handleRole = (role: any, response: any) => {
     createUser({
       variables: {
@@ -76,46 +76,46 @@ function Login() {
         email: response.profileObj.email,
         id: response.profileObj.googleId,
         imageUrl: response.profileObj.imageUrl,
-        plan: "Starter",
+        plan: 'Starter',
         role: role,
       },
-    });
+    })
     if (Gamecode) {
-      localStorage.setItem("user", JSON.stringify(response));
-      window.location.reload();
-      window.location = `/play?code=${Gamecode}` as any;
+      localStorage.setItem('user', JSON.stringify(response))
+      window.location.reload()
+      window.location = `/play?code=${Gamecode}` as any
     } else if (Secret && EmoteId) {
-      localStorage.setItem("user", JSON.stringify(response));
-      window.location.reload();
+      localStorage.setItem('user', JSON.stringify(response))
+      window.location.reload()
       window.location =
-        `/claim-emote?secret=${Secret}&emoteId=${EmoteId}` as any;
+        `/claim-emote?secret=${Secret}&emoteId=${EmoteId}` as any
     } else {
-      localStorage.setItem("user", JSON.stringify(response));
-      window.location.reload();
-      window.location.href = "/tutorial";
+      localStorage.setItem('user', JSON.stringify(response))
+      window.location.reload()
+      window.location.href = '/tutorial'
     }
-  };
+  }
   return (
     <div className="login-main-container">
       {step === 0 && (
         <div className="login-component-container">
           <br />
           <Typography variant="h3" className="login-title">
-            <b style={{ color: "#6C63FF" }}>{translations.login.title}</b>
+            <b style={{ color: '#6C63FF' }}>{translations.login.title}</b>
           </Typography>
           <br />
           <br />
           <Typography variant="h6" className="login-title">
-            <b style={{ fontStyle: "italic" }}>{translations.login.title2}</b>
+            <b style={{ fontStyle: 'italic' }}>{translations.login.title2}</b>
           </Typography>
           <br />
-          <div style={{ width: "100%", height: "auto", marginBottom: "40px" }}>
+          <div style={{ width: '100%', height: 'auto', marginBottom: '40px' }}>
             <img
               src={LoginArt}
               alt="login-art"
               style={{
-                width: "95%",
-                height: "300px",
+                width: '95%',
+                height: '300px',
               }}
             />
           </div>
@@ -124,18 +124,20 @@ function Login() {
             buttonText={translations.login.googlebutton}
             onSuccess={responseGoogle}
             onFailure={(error) => {
-              window.location = "/no-local-storage" as any;
+              console.log(error)
+              console.log('error was triggered')
+              window.location = '/no-local-storage' as any
             }}
-            cookiePolicy={"single_host_origin"}
-            style={{ backgroundColor: "#e0e0e0" }}
+            cookiePolicy={'single_host_origin'}
+            style={{ backgroundColor: '#e0e0e0' }}
             className="google-login-button"
             type="large"
           />
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '20px',
             }}
           >
             <a
@@ -143,8 +145,8 @@ function Login() {
               target="_blank"
               rel="noopener noreferrer"
               style={{
-                textDecoration: "underline",
-                color: "#6c63ff",
+                textDecoration: 'underline',
+                color: '#6c63ff',
               }}
             >
               {translations.login.reportissue}
@@ -157,11 +159,11 @@ function Login() {
           <Typography
             variant="h3"
             className="login-title"
-            style={{ color: "white" }}
+            style={{ color: 'white' }}
           >
             <b>{translations.login.role.title}</b>
           </Typography>
-          <div style={{ width: "100%", height: "100px" }} />
+          <div style={{ width: '100%', height: '100px' }} />
           <div className="role__img__wrapper">
             <div className="role__img__card">
               <Typography variant="h4" className="login-title">
@@ -172,7 +174,7 @@ function Login() {
                 alt="teacher"
                 className="login-role-img"
                 onClick={() => {
-                  handleRole("teacher", userObj);
+                  handleRole('teacher', userObj)
                 }}
               />
             </div>
@@ -185,7 +187,7 @@ function Login() {
                 alt="student"
                 className="login-role-img"
                 onClick={() => {
-                  handleRole("student", userObj);
+                  handleRole('student', userObj)
                 }}
               />
             </div>
@@ -193,6 +195,6 @@ function Login() {
         </div>
       )}
     </div>
-  );
+  )
 }
-export default Login;
+export default Login
--- src/hooks/getUser.ts
+++ src/hooks/getUser.ts
@@ -1,29 +1,30 @@
 const lsTest = () => {
-  if (typeof localStorage === "object") {
+  if (typeof localStorage === 'object') {
     try {
-      localStorage.setItem("localStorage", "1");
-      localStorage.removeItem("localStorage");
-      return true;
+      localStorage.setItem('localStorage', '1')
+      localStorage.removeItem('localStorage')
+      return true
     } catch (e) {
-      Storage.prototype._setItem = Storage.prototype.setItem;
-      Storage.prototype.setItem = function () {};
-      return false;
+      Storage.prototype._setItem = Storage.prototype.setItem
+      Storage.prototype.setItem = function () {}
+      return false
     }
   }
-};
+}
 const getUser = () => {
   if (lsTest() === false) {
-    window.location = "/no-local-storage" as any;
-    return null;
+    window.location = '/no-local-storage' as any
+    console.log('no local storage')
+    return null
   }
   try {
-    const user = JSON.parse(localStorage.getItem("user") as string);
-    if (user !== null && user !== undefined && user !== "") {
-      return user;
+    const user = JSON.parse(localStorage.getItem('user') as string)
+    if (user !== null && user !== undefined && user !== '') {
+      return user
     }
-    return null;
+    return null
   } catch (error) {
-    return null;
+    return null
   }
-};
-export default getUser;
+}
+export default getUser
